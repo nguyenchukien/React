eb8eee67f2203f4b816c855d3890020f17bb47f9
nguyenchukien <99160030+nguyenchukien@users.noreply.github.com>
2022-04-11 11:35:18 +0700

    first commit

---
+++ src/components/Create2.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Create2 = ({ text, number }) => {
+  return (
+    <div>
+      <div>
+        <span>{text}</span>
+      </div>
+    </div>
+  );
+};
+
+export default Create2;
---
+++ src/container/Contain4.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Contain4 = () => {
+  return (
+    <div>
+      <Create2 text=" PROJECT WORKING"></Create2>
+    </div>
+  );
+};
+
+export default Contain4;
